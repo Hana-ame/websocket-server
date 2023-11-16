fb35e6bafc649d621212c67c61f8e2a2a393e252
lumino <luminovoez@gmail.com>
2023-11-16 18:34:01 +0800

    cross compile

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ go build -o wss.exe
 export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=0
-# export CC=aarch64-linux-gnu-gcc
+# export CC="x86_64-linux-musl-gcc"
+export CGO_ENABLED=1
 
 go build -o wss.bin
